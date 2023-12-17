7a9f73a9fc4c52393c26e357e31d62fce0547a35
JasveerKaur129 <jasveerkaur129@gmail.com>
2023-12-17 20:05:17 +0530

    Merge branch 'main' into wip_new
    # Conflicts:
    #	.idea/example.java

--- .idea/example.java
+++ .idea/example.java
@@ -3,6 +3,12 @@ public class example {
     public static void main(String args[]){
 
         System.out.println("git practice");
-     
+        System.out.println("git ");
+
+
+        System.out.println("git ");
+
+
+
     }
 }
