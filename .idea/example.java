42c5420f1ca646c563ce47a4dd9786ec1e78b32d
JasveerKaur129 <jasveerkaur129@gmail.com>
2023-12-17 19:16:09 +0530

    project related to git

---
+++ .idea/example.java
@@ -0,0 +1,14 @@
+public class example {
+
+    public static void main(String args[]){
+
+        System.out.println("git practice");
+        System.out.println("git ");
+
+
+        System.out.println("git ");
+
+        System.out.println("git ");
+        System.out.println("git ");
+    }
+}
